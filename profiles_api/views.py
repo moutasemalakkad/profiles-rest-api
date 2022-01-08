eb9f8dbe132c570d0594c820a3403cf5435b30b1
moutasemalakkad <moutasemalakkad@gmail.com>
2022-01-08 09:48:06 -0500

    created a hello world view and added the url path

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+#from rest_framework import authentication, permissions
+#from django.contrib.auth.models import User
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test APIVIEW"""
+
+    def get(self, request, format=None):
+        """Return a list of API features """
+        an_apiview = [
+            'User an HTTP(get, post, patch, put, delete)',
+            'is similar to a tradational Django view'    ,
+            'gives you the most control over your app logic' ,
+            'mapped manually'
+        ]
+        return Response({
+                        'message' : 'hello world',
+                        'an_apiview' : an_apiview
+                        })
